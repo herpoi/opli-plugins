ca5d94831f849c45adbe81fc88e8ad06653da18d
unknown <zdzislaw22@windowslive.com>
2013-07-08 08:43:10 +0200

    cleanup

--- BoardReader/_version.py
+++
@@ -1,9 +0,0 @@
-#QQ.XX.YY.ZZ
-#QQ-new player added
-#XX-new functionality added
-#YY-improvement
-#ZZ-minor changes
-#changing YY requries setting ZZ to zeros
-#changing XX requries setting YY and ZZ to zeros
-#changing QQ requries setting XX and YY and ZZ to zeros
-version="01.00.00.00"
