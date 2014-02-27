9d62c5d2ac4f88bbcf2578b69a67035d876ba6fa
unknown <zdzislaw22@windowslive.com>
2014-02-27 08:28:23 +0100

    xunil.pl next commit

--- BoardsClient/libs/smf.py
+++ BoardsClient/libs/smf.py
@@ -172,7 +172,7 @@ def GetForumsList(WebPage = -1):
             Forums.append({'ID': forumID,'LEVEL': forumlevel,'NAME': forumName, 'ParenID': parentID})
             UsedIDs.append(forumID)
     for forum in Forums
-        Txt2Search = '<tr id="board_' + str(forum['ID']) + "_children"
+        Txt2Search = 'tr id="board_' + str(forum['ID']) + "_children"
         if WebPage.find(Txt2Search) > 0: 
             SubPage = WebPage[WebPage.find(Txt2Search) + len(Txt2Search):]
             SubPage = SubPage[:SubPage.find('</td>')]
