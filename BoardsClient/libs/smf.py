808ee128b83b4b16af5fd5e5ac68ff7c7cc7fee1
unknown <zdzislaw22@windowslive.com>
2014-02-27 18:45:03 +0100

    xunil.pl next commit

--- BoardsClient/libs/smf.py
+++ BoardsClient/libs/smf.py
@@ -138,7 +138,7 @@ def GetThreadsList(WebPage = -1 ):
     for thread in ThreadsList:
         #print thread
         threadID = int(thread[0])
-        threadTITLE = thread[1]
+        threadTITLE = "%s" % thread[1]
         threadICON = thread[2]
         threadDESCR = ''
         printDBG('threadID:'+ str(threadID) + '\nthreadICON:' + threadICON + '\nthreadTITLE:'+ threadTITLE + '\nthreadDESCR:' + threadDESCR + '\n')
