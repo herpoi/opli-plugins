665f51d9268322853f1acc8c3b8e3848e4d38670
unknown <zdzislaw22@windowslive.com>
2014-02-27 08:29:13 +0100

    xunil.pl next commit

--- BoardsClient/libs/smf.py
+++ BoardsClient/libs/smf.py
@@ -171,7 +171,7 @@ def GetForumsList(WebPage = -1):
         if forumName != '' and forumID not in UsedIDs and forumID not in BlockedIDs and parentID not in BlockedIDs:
             Forums.append({'ID': forumID,'LEVEL': forumlevel,'NAME': forumName, 'ParenID': parentID})
             UsedIDs.append(forumID)
-    for forum in Forums
+    for forum in Forums:
         Txt2Search = 'tr id="board_' + str(forum['ID']) + "_children"
         if WebPage.find(Txt2Search) > 0: 
             SubPage = WebPage[WebPage.find(Txt2Search) + len(Txt2Search):]
